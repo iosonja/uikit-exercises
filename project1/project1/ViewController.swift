72c76465f9c49896f99f1a8d688b8ff89e477380
Sonja Ek <sonja3k@icloud.com>
2021-02-19 13:11:48 +0200

    Add functionality for fetching images

--- project1/project1/ViewController.swift
+++ project1/project1/ViewController.swift
@@ -8,19 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let fm = FileManager.default
-        let path = Bundle.main.resourcePath!
+        let path = Bundle.main.resourcePath! // This is where all the assets are
         let items = try! fm.contentsOfDirectory(atPath: path)
         
         for item in items {
             if item.hasPrefix("nssl") {
-                //
+                pictures.append(item)
             }
         }
+        
+        print(pictures)
     }
 
 
