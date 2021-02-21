03a6bb58bd4749b712ddfb3c49a65d19a4a8738b
Sonja Ek <sonja3k@icloud.com>
2021-02-21 19:30:45 +0200

    Create project 2 and add some UI elements

---
+++ project2/project2/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  project2
+//
+//  Created by Sonja Ek on 21.2.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+    
+        askQuestion()
+    }
+    
+    func askQuestion() {
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+    }
+
+
+}
+
