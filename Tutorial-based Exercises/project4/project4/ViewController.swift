c35f286e16dd066168321cd1664c63f23c1d813e
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-03-01 20:35:35 +0200

    Add UIAlertController for choosing between different websites to load

--- Tutorial-based Exercises/project4/project4/ViewController.swift
+++ Tutorial-based Exercises/project4/project4/ViewController.swift
@@ -21,9 +21,33 @@ class ViewController: UIViewController, WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let url = URL(string: "https://www.sonjaek.com")! // Force unwrapping is ok bc this is hand-typed
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open",
+                                                            style: .plain,
+                                                            target: self,
+                                                            action: #selector(openTapped))
+        
+        let url = URL(string: "https://www.apple.com")! // Force unwrapping is ok bc this is hand-typed
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+    }
+    
+    @objc func openTapped() {
+        let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "sonjaek.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem // for iPad
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle = action.title else { return }
+        guard let url = URL(string: "https://" + actionTitle) else { return }
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
