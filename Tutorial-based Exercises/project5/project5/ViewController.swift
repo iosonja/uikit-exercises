a9eb327ae40147a7adb4e8420253c23f4b9d876f
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-03-08 21:11:29 +0200

    Add popup text field for user input

--- Tutorial-based Exercises/project5/project5/ViewController.swift
+++ Tutorial-based Exercises/project5/project5/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add,
+                                                            target: self,
+                                                            action: #selector(promptForAnswer))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -35,10 +39,32 @@ class ViewController: UITableViewController {
         tableView.reloadData()
     }
     
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView,
+                            cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
+    }
+    
+    @objc func promptForAnswer() {
+        let ac = UIAlertController(title: "Enter answer",
+                                   message: nil,
+                                   preferredStyle: .alert)
+        ac.addTextField() // This is wehere user puts their answers
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            // Avoid strong reference cycle, very important for performance:
+            [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        
     }
 
 }
