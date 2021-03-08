3d2f6c821cbf430b1d7da36b7b93041c9033988c
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-03-08 20:21:01 +0200

    Replace file template for project5

---
+++ Tutorial-based Exercises/project5/project5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project5
+//
+//  Created by Sonja Ek on 8.3.2021.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
