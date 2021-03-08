b969eb0aa103a951786253227ef8cdbab85b11ac
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-03-08 20:28:29 +0200

    Load words from start.txt

--- Tutorial-based Exercises/project5/project5/ViewController.swift
+++ Tutorial-based Exercises/project5/project5/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var allWords = [String]()
+    var usedWords = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                allWords = startWords.components(separatedBy: "\n")
+            }
+        }
+        
+        if allWords.isEmpty { // More efficient than counting length
+            allWords = ["silkworm"]
+        }
     }
 
 
