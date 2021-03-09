499883654dd71c2b73f1f788bdf71200c5814607
seekaboo <40118812+seekaboo@users.noreply.github.com>
2021-03-09 19:54:08 +0200

    Treat answers as lowercased when adding to list

--- Tutorial-based Exercises/project5/project5/ViewController.swift
+++ Tutorial-based Exercises/project5/project5/ViewController.swift
@@ -77,7 +77,7 @@ class ViewController: UITableViewController {
             if isOriginal(word: lowerAnswer) {
                 if isReal(word: lowerAnswer) {
                     if isNew(word: lowerAnswer) {
-                        usedWords.insert(answer, at: 0)
+                        usedWords.insert(lowerAnswer, at: 0)
                         
                         let indexPath = IndexPath(row: 0, section: 0)
                         tableView.insertRows(at: [indexPath], with: .automatic)
