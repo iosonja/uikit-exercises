c92f5b66437b4b6d4586a710a22ac59a39d04245
Sonja Ek <sonja3k@icloud.com>
2021-03-17 19:58:58 +0200

    Create project 7 with a ready-ish UI

---
+++ Tutorial-based Exercises/project7/project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  project7
+//
+//  Created by Sonja Ek on 17.3.2021.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Tutorial-based Exercises/project7/project7/Petitions.swift
@@ -0,0 +1,11 @@
+//
+//  Petitions.swift
+//  project7
+//
+//  Created by Sonja Ek on 17.3.2021.
+//
+
+import Foundation
+struct Petitions: Codable {
+    var results: [Petition]
+}
